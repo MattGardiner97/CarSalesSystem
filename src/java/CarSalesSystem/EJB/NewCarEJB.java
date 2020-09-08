d6f70abadfdd29313ac210301736b5dc5eb010c3
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-09-08 19:13:44 +1000

    Implemented basic EJB logic

--- src/java/CarSalesSystem/EJB/NewCarEJB.java
+++ src/java/CarSalesSystem/EJB/NewCarEJB.java
@@ -1,10 +1,21 @@
 package CarSalesSystem.EJB;
 
+import CarSalesSystem.Entities.NewCar;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
-public class CarEJB {
+public class NewCarEJB {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext(unitName = "CarSalesSystemPU")
+    private EntityManager em;
+    
+    public NewCar createNewCar(NewCar car){
+        em.persist(car);
+        return car;
+    }
+
 }
